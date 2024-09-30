fbede6daf5a89464cf4f4130a963aace52802726
Taashev <taashev.a.r92@yandex.ru>
2024-09-30 17:20:24 +0300

    DEV-8: настрайка gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { StoreContext } from './context/StoreContext';
 import { rootStore } from './store/RootStore';
@@ -15,10 +15,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <StoreContext.Provider value={rootStore}>
         <App />
       </StoreContext.Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
